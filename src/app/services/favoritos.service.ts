89a368ea2b29b26a5c69d37b333d772fd7bb7c61
Oliver Aton <oliver.gil.estrada9@gmail.com>
2024-03-20 18:55:01 -0600

    Heloo

--- src/app/services/favoritos.service.ts
+++ src/app/services/favoritos.service.ts
@@ -10,6 +10,14 @@ export class FavoritosService {
   constructor() { }
 
   addFavorito(card: PokemonData){
+    const encontrado = this.misFavoritos.find(pokemon => pokemon.id === card.id);
+    console.log(encontrado);
+    if(encontrado){
+      const  filtrados = this.misFavoritos.filter(c => c.id !== encontrado.id);
+      console.log(filtrados);
+      localStorage.setItem('favoritos', JSON.stringify(this.misFavoritos));
+      return;
+    }
     this.misFavoritos.unshift(card);
     localStorage.setItem('favoritos', JSON.stringify(this.misFavoritos));
   }
